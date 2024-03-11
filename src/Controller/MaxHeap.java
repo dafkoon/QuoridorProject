b73682e086af9c9d3b8ccfabaede0e9f2539b8f1
dafkoon <dtozik0608@gmail.com>
2024-03-11 22:53:59 +0200

    sdads
    sadadsa

--- src/Controller/MaxHeap.java
+++
@@ -1,9 +0,0 @@
-package Controller;
-import java.util.PriorityQueue;
-import java.util.Comparator;
-public class MaxHeap {
-    private final PriorityQueue<String> queue;
-    public MaxHeap() {
-        this.queue = new PriorityQueue<>(Comparator.reverseOrder());
-    }
-}
